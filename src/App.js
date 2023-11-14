8671bf1b452fa7061f32f59fda851d8b239ee491
miloupark <helenapark96@naver.com>
2023-11-14 13:17:43 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, HashRouter as Router } from 'react-router-dom'
 import Home from './pages';
 import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import GlobalStyles from './assets/styles/GlobalStyles.styles';
@@ -15,14 +15,14 @@ function App() {
         <ChakraProvider theme={theme}>
             <CSSReset />
             <GlobalStyles />
-            <Routes>
+            <Router>
                 <Route path="/" element={<Home />} />
                 <Route path="/marketplace" element={<MarketPlace />} />
                 <Route path="/tables" element={<Tables />} />
                 <Route path="/kanban" element={<Kanban />} />
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/signin" element={<SignIn />} />
-            </Routes>
+            </Router>
         </ChakraProvider>
     );
 }
