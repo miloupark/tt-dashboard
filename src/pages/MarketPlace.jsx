cca7282f066fd3eb5260dd628f6204df1e05d06f
miloupark <helenapark96@naver.com>
2023-11-17 11:02:04 +0900

    api

--- src/pages/MarketPlace.jsx
+++ src/pages/MarketPlace.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
-import { Flex, Grid } from '@chakra-ui/react';
+import { Box, Flex, Grid, SimpleGrid } from '@chakra-ui/react';
+import Banner from '../components/marketplace/Banner';
+import Header from '../components/layout/header/Header';
+import NFTCollection from '../components/marketplace/NFTCollection';
+
 const MarketPlace = () => {
     return (
         <Layout title={'NFT MarketPlace'} pagename={'MarketPlace'}>
@@ -9,15 +13,15 @@ const MarketPlace = () => {
                 gap={{ base: '20px', xl: '20px' }}
                 display={{ base: 'block', xl: 'grid' }}
             >
-                <Flex
-                    flexDirection="column"
-                    gridArea={{ xl: '1 / 1 / 2 / 3', '2xl': '1 / 1 / 2 / 2' }}
-                    w="100%"
-                    h="300px"
-                    bg="red"
-                    mb={'20px'}
-                    
-                ></Flex>
+                <Flex flexDirection="column" gridArea={{ xl: '1 / 1 / 2 / 3', '2xl': '1 / 1 / 2 / 2' }} mb={'20px'}>
+                    <Banner />
+                    <Box>
+                        <Header>Trending NFTs</Header>
+                        <SimpleGrid columns={[1, 2, null, 3]} spacing={'20px'} mt={'20px'}>
+                            <NFTCollection/>
+                        </SimpleGrid>
+                    </Box>
+                </Flex>
                 <Flex
                     flexDirection="column"
                     gridArea={{ xl: '1 / 3 / 2 / 4', '2xl': '1 / 2 / 2 / 3' }}
@@ -29,4 +33,5 @@ const MarketPlace = () => {
         </Layout>
     );
 };
+
 export default MarketPlace;
